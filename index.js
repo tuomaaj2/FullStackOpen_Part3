6538d30f0e51673e67639f8b3930c7660272a9e8
Juho-Topias Tuomaala <juho-topias@hotmail.com>
2021-09-14 14:06:37 +0300

    3.3 completed

--- index.js
+++ index.js
@@ -34,6 +34,17 @@ app.get('/info', (req, res) => {
     res.write(new Date().toString())
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
