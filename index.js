f67239fa3bf7d9a5d2254ff8ef8a1ae244b4735c
Juho-Topias Tuomaala <juho-topias@hotmail.com>
2021-09-21 11:49:02 +0300

    3.11 completed

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :resp'))
 
 let persons = [
